90660f54aac151b79ab5b13dce1bc92726bdd1a3
David <david.shams@gmail.com>
2019-08-26 14:59:22 -0500

    grid-template-columns

--- src/App.js
+++ src/App.js
@@ -3,7 +3,19 @@ import './App.css';
 
 function App() {
   return (
-    <div>CSS React</div>
+    <div className="mgrid">
+      CSS React
+      <div className="item">One</div>
+      <div className="item">1-2</div>
+      <div className="item">1-3</div>
+      <div className="item">1-4</div>
+      <div className="item">2-1</div>
+      <div className="item">2-2</div>
+      <div className="item">2-3</div>
+      <div className="item">2-4</div>
+      <div className="item">3-1</div>
+
+    </div>
   );
 }
 
