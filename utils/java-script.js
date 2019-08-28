0178bed48b7852e3450a05c5cd43035b7e716b55
David <david.shams@gmail.com>
2019-08-27 20:55:58 -0500

    get user input from comand line in JavaScript

--- utils/java-script.js
+++ utils/java-script.js
@@ -13,6 +13,31 @@ console.log(Math.abs(-127)); // 127
 console.log(Math.sqrt(10)); // 3.1622776601683795
 console.log(Math.sqrt(10).toFixed(3)); // 3.1622776601683795
 
+/**
+ * READ USER INPUT
+ * 
+ */
+// Get process.stdin as the standard input object.
+var standard_input = process.stdin;
+// Set input character encoding.
+standard_input.setEncoding('utf-8');
+// Prompt user to input data in console.
+console.log("Please input text or type `exit`: ");
+// When user input data and click enter key.
+standard_input.on('data', function (data) {
+    // User input exit.
+    if(data === 'exit\n'){
+        // Program exit.
+        console.log("User input complete, program exit.");
+        process.exit();
+    }else
+    {
+        // Print user input in console.
+        console.log('User Input Data : ' + data);
+    }
+});
+
+
 let id = 10;
 switch(id){
     case 5:
@@ -25,4 +50,11 @@ switch(id){
 
     default:
         console.log('none');
+}
+if( id===5 ) {
+    console.log('5');
+} else if(id===10) {
+    console.log('10');
+} else {
+    console.log('none');
 }
