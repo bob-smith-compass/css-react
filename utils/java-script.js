9547eb59d7d6c723e2f53265761cc955e9b3514e
David <david.shams@gmail.com>
2019-08-27 20:45:16 -0500

    toFixed switch

--- utils/java-script.js
+++ utils/java-script.js
@@ -10,3 +10,19 @@ console.log(i); // 5
  */
 console.log(Math.sqrt(81)); // 9
 console.log(Math.abs(-127)); // 127
+console.log(Math.sqrt(10)); // 3.1622776601683795
+console.log(Math.sqrt(10).toFixed(3)); // 3.1622776601683795
+
+let id = 10;
+switch(id){
+    case 5:
+        console.log('5');
+        break;
+
+    case 10:
+        console.log('10');
+        break;
+
+    default:
+        console.log('none');
+}
