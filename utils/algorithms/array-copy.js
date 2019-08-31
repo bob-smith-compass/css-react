e7304907c69c807e7ba54aa9ce2bd44269e76264
David <david.shams@gmail.com>
2019-08-31 15:33:24 -0500

    ES6 OR NODE OR SOMETHING makes a deep copy of the array NOW!

--- utils/algorithms/array-copy.js
+++ utils/algorithms/array-copy.js
@@ -5,3 +5,9 @@ console.log('b', b);
 a = [];
 console.log('a', a);
 console.log('b', b); // b [ 1, 2, 3, 4 ]
+
+for (let i = 0; i < 10; i++) {
+    a[i] = i;
+}
+console.log('a', a);
+console.log('b', b);
