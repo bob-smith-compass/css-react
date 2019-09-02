b5934fffefe2fa7de104e2f54d28cbf0a498dcd0
David <david.shams@gmail.com>
2019-09-01 20:53:10 -0500

    delete object property

--- utils/algorithms/delete.js
+++ utils/algorithms/delete.js
@@ -5,5 +5,7 @@ let o = {
 };
 
 console.log(o);
-delete o; // Parsing error: Deleting local variable in strict mode
-console.log(o);
+// delete o; // Parsing error: Deleting local variable in strict mode
+delete o.me; // Parsing error: Deleting local variable in strict mode
+console.log(o); // {}
+// console.log(o.me.first); // undefined
