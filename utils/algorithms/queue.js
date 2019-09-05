b3413e1968e3b2e6338cee7bfe572e29a2c61808
David <david.shams@gmail.com>
2019-09-04 21:26:31 -0500

    QUEUE  * 3. shift() - remove from fron of array and return ELEMENT  * 4. unshift() - add to the fron of array and return SIZE

---
+++ utils/algorithms/queue.js
@@ -0,0 +1,20 @@
+/**
+ * Queue
+ * 1. enqueue
+ * 2. dequeue
+ * 3. shift() - remove from fron of array and return ELEMENT
+ * 4. unshift() - add to the fron of array and return SIZE
+ */
+class Queue {
+    constructor(){
+        this._queue = [];
+    }
+    enqueue(e) {
+        this._queue.push(e);
+    }
+    dequeue(e) {
+        this._queue.shift() {
+
+        }
+    }
+}
