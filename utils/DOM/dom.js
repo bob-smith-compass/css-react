217bd210711402d802b5d0062f4a184fa6c1ffbb
David <david.shams@gmail.com>
2019-09-05 20:56:25 -0500

    true: let parent handle event first addEventListener('click', handleClick, true);

--- utils/DOM/dom.js
+++ utils/DOM/dom.js
@@ -1,7 +1,14 @@
 let handler = (e) => {
     console.log(e);
 };
+let handleClick = (e) => {
+    console.log(e.target);
+    console.log(e);
+    console.log(this.id);
+};
 document.getElementById('me').addEventListener('click', handler, false);
+document.getElementById('red').addEventListener('click', handleClick, true);
+document.getElementById('blue').addEventListener('click', handleClick, true);
 
 let removeHandler = (e) => {
     e.preventDefault();
