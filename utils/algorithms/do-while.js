42553d32a4f889ba61bb7191d0020a5ffd5edbf1
David <david.shams@gmail.com>
2019-09-07 14:43:43 -0500

    doubly-linked-list

--- utils/algorithms/do-while.js
+++ utils/algorithms/do-while.js
@@ -1,10 +1,22 @@
 let a = [true, false, null, true, false];
 a = [true, false, , true, false]; // will put your loop into infinite loop
+for(let i = 0; i<a.length; i++) {
+    if(a[i] === undefined){
+        console.log('UNDEFINED');
+    } else {
+        console.log(a[i]);
+    }
+}
+console.log(a[2]) ;
+console.log((a[2] !== undefined));
 console.log(a);
 let i = 0;
-do {
-    console.log(a[a.length-i]);
-    i++;
-} while(a[a.length-i] !== null);
+if(a[a.length-i] !== undefined){
+    do {
+        console.log(a[a.length-i]);
+        i++;
+
+    } while((a[a.length-i] !== undefined) && a[a.length-i] !== null);
+}
 
 console.log(`All elements in reverse`);
