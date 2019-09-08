5b6fdce063d67ac9d48f0b6c493b51ce25b1150a
David <david.shams@gmail.com>
2019-09-08 14:03:16 -0500

    DEBUG: clusure

--- utils/algorithms/closures.js
+++ utils/algorithms/closures.js
@@ -1,7 +1,7 @@
 var a = {};
 
-for(var i = 0; i<3; i++) {
-    a[i] = function() {
+for (var i = 0; i < 3; i++) {
+    a[i] = function () {
         // alert(i);
         console.log(i);
     }
@@ -12,10 +12,20 @@ a[2](); // 3
 
 var counter = 0;
 
-let f = function(){
+let f = function () {
     return counter++;
 }
 counter += 1;
 console.log(f());
 console.log(f());
 console.log(f());
+
+let b_closure = {};
+for (let i = 0; i < 3; i++) {
+    b_closure[i] = (function (i) {
+        console.log(i);
+    })() // IFFE closure
+}
+b_closure[0]();
+b_closure[1]();
+b_closure[2]();
